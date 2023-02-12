e64a5d51d95bcec8bd5aa258f153656ad8af27a2
polacomark <you@example.com>
2023-02-12 11:18:02 -0300

    actualizacion

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -47,35 +47,35 @@
         php_email_form_submit(thisForm, action, formData);
       }
     });
-  });
+   });
 
   function php_email_form_submit(thisForm, action, formData) {
-    fetch(action, {
-      method: 'POST',
-      body: formData,
-      headers: {'X-Requested-With': 'XMLHttpRequest'}
-    })
-    .then(response => {
-      return response.text();
-    })
-    .then(data => {
-      thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
-        thisForm.querySelector('.sent-message').classList.add('d-block');
-        thisForm.reset(); 
-      } else {
-        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
-      }
-    })
-    .catch((error) => {
-      displayError(thisForm, error);
-    });
-  }
+    // fetch(action, {
+    //   method: 'POST',
+    //   body: formData,
+    //   headers: {'X-Requested-With': 'XMLHttpRequest'}
+    // })
+    // .then(response => {
+    //   return response.text();
+    // })
+    // .then(data => {
+    //   thisForm.querySelector('.loading').classList.remove('d-block');
+    //   if (data.trim() == 'OK') {
+    //     thisForm.querySelector('.sent-message').classList.add('d-block');
+    //     thisForm.reset(); 
+    //   } else {
+    //     throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
+    //   }
+    // })
+    // .catch((error) => {
+    //   displayError(thisForm, error);
+    // });
+   }
 
   function displayError(thisForm, error) {
     thisForm.querySelector('.loading').classList.remove('d-block');
     thisForm.querySelector('.error-message').innerHTML = error;
     thisForm.querySelector('.error-message').classList.add('d-block');
   }
-
+  
 })();
